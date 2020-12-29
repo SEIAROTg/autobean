2f3e808362a4167693b3d36b74935c73b82c10a2
SEIAROTg <seiarotg@gmail.com>
2020-12-29 22:11:16 +0000

    fix: fix broken tests due to upstream change in https://github.com/beancount/beancount/issues/281

--- autobean/utils/compare.py
+++ autobean/utils/compare.py
@@ -7,10 +7,13 @@ from beancount.core.compare import hash_entry
 
 # This is similar to beancount.core.comparer.compare_entries but allows duplicated entries
 def compare_entries(entries1: List[Directive], entries2: List[Directive]):
-    hashes1 = {hash_entry(entry): entry for entry in entries1}
-    hashes2 = {hash_entry(entry): entry for entry in entries2}
-    keys1 = Counter(hash_entry(entry) for entry in entries1)
-    keys2 = Counter(hash_entry(entry) for entry in entries2)
+    hashes1 = {
+        hash_entry(entry, exclude_meta=True): entry for entry in entries1}
+    hashes2 = {
+        hash_entry(entry, exclude_meta=True): entry for entry in entries2}
+
+    keys1 = Counter(hash_entry(entry, exclude_meta=True) for entry in entries1)
+    keys2 = Counter(hash_entry(entry, exclude_meta=True) for entry in entries2)
 
     same = keys1 == keys2
     missing1 = data.sorted([hashes1[key] for key in keys1 - keys2])
