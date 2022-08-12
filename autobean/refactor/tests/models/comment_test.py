d7bc8e1de2e38f6e9744e43aa96b0508c5dbe9a4
SEIAROTg <seiarotg@gmail.com>
2022-08-12 18:04:28 +0100

    fix(refactor): only match indented comment line as COMMENT_LINE.

--- autobean/refactor/tests/models/comment_test.py
+++ autobean/refactor/tests/models/comment_test.py
@@ -1,8 +1,60 @@
+import pytest
+from lark import exceptions
 from autobean.refactor import models
 from . import base
 
 
 class TestComment(base.BaseTestModel):
+
+    @pytest.mark.parametrize(
+        'text', [
+            ';foo',
+            '; foo',
+            '; 你好!',
+            ';',
+            ';"',
+            ';""',
+        ],
+    )
+    def test_parse_success_inline(self, text: str) -> None:
+        token = self.parser.parse_token(text, models.InlineComment)
+        assert token.raw_text == text
+        self.check_deepcopy_token(token)
+
+    @pytest.mark.parametrize(
+        'text', [
+            ' ;foo',
+            ' ;foo\n',
+        ],
+    )
+    def test_parse_success_line(self, text: str) -> None:
+        token = self.parser.parse_token(text, models.LineComment)
+        assert token.raw_text == text
+        self.check_deepcopy_token(token)
+
+    @pytest.mark.parametrize(
+        'text', [
+            '\n;foo',
+            '',
+            ' ;foo',
+            ' ;foo\n',
+            ';foo\n',
+        ],
+    )
+    def test_parse_failure_inline(self, text: str) -> None:
+        with pytest.raises(exceptions.UnexpectedInput):
+            self.parser.parse_token(text, models.InlineComment)
+
+    @pytest.mark.parametrize(
+        'text', [
+            '\n;foo',
+            '',
+            ';foo',
+        ],
+    )
+    def test_parse_failure_line(self, text: str) -> None:
+        with pytest.raises(exceptions.UnexpectedInput):
+            self.parser.parse_token(text, models.LineComment)
 
     def test_all_comments_in_file(self) -> None:
         text = '''\
